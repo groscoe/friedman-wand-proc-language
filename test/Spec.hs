e560f1d80d7eeb18b26327063a090ddc9e6c9f8d
groscoe <gustavo.roscoe@waycarbon.com>
2018-06-03 23:24:46 -0300

    Added a few test cases

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,53 +3,56 @@ module Test where
 import Eval (run)
 
 -- examples
-ex0, ex1, ex2, ex3, ex4, ex5, ex6, ex7, ex8, ex9 :: String
-ex0 = "let x = 3 in -(55, -(x, 11))"
+ex0, ex1, ex2, ex3, ex4, ex5, ex6, ex7 :: String
 
-ex1 = "let x = 5 in -(x, 3)"
+-- 55
+ex0 = "let f = proc (x) -(x,11)\n\
+      \in (f (f 77))"
 
-ex2 =
-  "let z = 5\n\
-  \in let x = 3\n\
-  \   in let y = -(x, 1)\n\
-  \      in let x = 4\n\
-  \         in -(z, -(x, y))"
+-- 55
+ex1 = "(proc (f) (f (f 77))\n\
+       \proc (x) -(x,11))"
 
-ex3 =
-  "let x = 7\n\
-  \in let y = 2\n\
-  \   in let y = let x = -(x, 1)\n\
-  \              in -(x, y)\n\
-  \      in -(-(x,8), y)"
+-- -100
+ex2 = "let x = 200\n\
+      \in let f = proc (z) -(z,x)\n\
+      \  in let x = 100\n\
+      \     in let g = proc (z) -(z,x)\n\
+      \        in -((f 1), (g 1))"
 
-ex4 = "let x = 33\n\
-      \in let y = 22\n\
-      \   in if zero?(-(x,11))\n\
-      \      then -(y,2)\n\
-      \      else -(y,4)"
+-- 7
+ex3 = "let sum = proc (x) proc (y) +(x, y)\n\
+      \in ((sum 3) 4)"
 
-ex5 =
-  "let x = 4\n\
-  \in cons(x,\n\
-  \          cons(cons(-(x,1),\n\
-  \                    emptylist),\n\
-  \               emptylist))"
+-- 12
+ex4 = "let makemult = proc (maker)\n\
+      \                 proc (x)\n\
+      \                   if zero?(x)\n\
+      \                   then 0\n\
+      \                   else -(((maker maker) -(x,1)), -4)\n\
+      \in let times4 = proc (x) ((makemult makemult) x)\n\
+      \   in (times4 3)"
 
-ex6 = "let x = 4\n\
-      \in list(x, -(x,1), -(x,3))"
+-- 12
+ex5 = "let makerec = proc (f)\n\
+      \                     let d = proc (x)\n\
+      \                              proc (z) ((f (x x)) z)\n\
+      \                     in proc (n) ((f (d d)) n)\n\
+      \in let maketimes4 = proc (f) proc (x)\n\
+      \                            if zero?(x)\n\
+      \                            then 0\n\
+      \                            else -((f -(x,1)), -4)\n\
+      \   in let times4 = (makerec maketimes4) in (times4 3)"
 
-ex7 =
-  "let x = 30\n\
-  \in let x = -(x, 1)\n\
-  \       y = -(x, 2)\n\
-  \   in -(x, y)"
+-- 6
+ex6 = "let a = 3\n\
+      \in let p = proc (x) -(x,a)\n\
+      \             a = 5\n\
+      \   in -(a,(p 2))"
 
-ex8 =
-  "let x = 30\n\
-  \in let* x = -(x,1) y = -(x,2)\n\
-  \   in -(x,y)"
-
-ex9 =
-  "let u = 7\n\
-  \in unpack x y = cons(u,cons(3,emptylist))\n\
-  \   in -(x,y)"
+-- 3
+ex7 = "let a = 3\n\
+      \in let p = proc (z) a\n\
+      \   in let f = proc (x) (p 0)\n\
+      \      in let a = 5\n\
+      \         in (f 2)"
